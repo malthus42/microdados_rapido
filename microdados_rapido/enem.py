61909a6648e54857eadad8a648fcb9bcb8d340a8
Matheus <matheusslcosta@usp.br>
2026-06-02 14:15:31 -0300

    add codigo matriz respostas

--- microdados_rapido/enem.py
+++ microdados_rapido/enem.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import numpy as np
+import pandas as pd
 
 class ENEMMixin:
 
@@ -41,3 +43,53 @@ class ENEMMixin:
         """
 
         return self.query(query)
+    
+    def matriz_respostas(
+    self, 
+    df, 
+    area, 
+    numero_inicial, 
+    n_itens=45, 
+    offset_resposta=0, 
+    offset_gabarito=0
+    ):
+
+        respostas_col = f"TX_RESPOSTAS_{area}"
+        gabarito_col = f"TX_GABARITO_{area}"
+
+        if df.empty:
+            raise ValueError("DataFrame vazio.")
+
+        gabarito_base = df[gabarito_col].iloc[0]
+
+        respostas_base = df[respostas_col].astype(str)
+
+        matriz_binaria = pd.DataFrame(index=df.index)
+
+        item_cols = []
+
+        for i in range(n_itens):
+
+            numero_questao = numero_inicial + i
+
+            item_id = f"{area}_Item_{numero_questao}"
+
+            item_cols.append(item_id)
+
+            coluna_resposta = respostas_base.str[i + offset_resposta]
+
+            gabarito_char = gabarito_base[i + offset_gabarito]
+
+            is_correct = coluna_resposta == gabarito_char
+
+            is_valid = coluna_resposta.isin(
+                ['A', 'B', 'C', 'D', 'E']
+            )
+
+            matriz_binaria[item_id] = np.where(
+                is_valid,
+                is_correct.astype(int),
+                np.nan
+            )
+
+        return matriz_binaria
